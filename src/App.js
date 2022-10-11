510cfedede634c3aeb3fb5938f1c3cdd2447d516
FCarter09 <fc_vstate@yahoo.com>
2022-10-11 13:20:11 -0400

    feature/comntact

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
-import Gallery from "./components/Gallery";
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   // the useState hook is a function that will always return an array. 
   const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
+    {name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects',},
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
@@ -27,6 +25,7 @@ function App() {
         currentCategory={currentCategory}>
         </Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main> 
